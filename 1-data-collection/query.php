e0ebcf0803ef2635fcd4935c1b1290d2b39525ff
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-09-29 23:53:07 +0800

    Get new list with like counts.

--- 1-data-collection/query.php
+++ 1-data-collection/query.php
@@ -1,32 +1,59 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once 'safe_facebook.php';
+
+$fp = fopen('target_list_serialized.dat', 'r');
+$unserialized = fread($fp, filesize('target_list_serialized.dat'));
+fclose($fp);
+$targetList = unserialize($unserialized);
 
 // destroy these before making the source code public
 $secretKey = '3a9c3467214697db53d6ff29a96f52d7';
-$accessToken = 'EAARZCIAZCAqZA4BAAZBZBBBQ4ssGXohyK4gltqUIUIs9WGiv3soZCerJZAI2NgYBt'.
-               'UZBGMzXTTMDu4Xj5KJNnNnEi5dlcveO1ZCF2KfYwHmopySSXCm1l4unAR43EJDc777'.
-               '7JW0SQahjrkF1h5hS2FvappGjbjFu64Et3xmwOV9YZB0k8bWOcSPHGF';
+$accessToken = 'EAARZCIAZCAqZA4BAKqTcW8RUkJOgaEIrOrS22UMzXiqZCM95OLTa1mrmxhzPd2ZCTwfWBDD4fYPgbFJZBZCl37iLxDtjjWZBbTxS'.
+               '61mxsL5VGrZAwRG1fMQZCES3mQi3uyhcsQwFUIs3wjZBdxbCm5CDsVIahpFfzreIgPNhWdTsaWludJuKsg0j1eMNNrGdD3e2uYZD';
 
-$fb = new Facebook\Facebook([
+$fb = new SafeFacebook\SafeFacebook([
 	'app_id' => '1265675586808222',
 	'app_secret' => $secretKey,
 	'default_graph_version' => 'v2.7',
 ]);
 
+// $oauth = $fb->getOAuth2Client();
+// $accessToken = $oauth->getLongLivedAccessToken($accessToken);
+// print_r($accessToken);
 $fb->setDefaultAccessToken($accessToken);
 
-try {
-	$response = $fb->get('/me?fields=id,name');
-	$plainArray = $response->getDecodedBody();
-	$userNode = $response->getGraphUser();
-}
-catch(Facebook\Exceptions\FacebookResponseException $e) {
-	die('Graph API Error: ' . $e->getMessage() . PHP_EOL);
-}
-catch(Facebook\Exceptions\FacebookSDKException $e) {
-	die('Facebook SDK Error: ' . $e->getMessage(). PHP_EOL);
+$fullTargetList = array();
+foreach ($targetList as $item) {
+	try {
+		$response = $fb->get('/search?q=' . rawurlencode($item) . '&type=page');
+		$targetId = ($response->getGraphEdge()->asArray())[0]['id'];
+		if ($targetId) {
+			$response = $fb->get('/' . $targetId . '?fields=name,fan_count');
+			$target = $response->getGraphNode()->asArray();
+			$fullTargetList[] = $target;
+			print_r($target);
+		}
+		else
+			echo '"' . $item . '" not found' . PHP_EOL;
+	}
+	catch (Facebook\Exceptions\FacebookResponseException $e) {
+		echo 'Graph API Error: ' . $e->getMessage() . PHP_EOL;
+	}
+	catch (Facebook\Exceptions\FacebookSDKException $e) {
+		echo 'Facebook SDK Error: ' . $e->getMessage() . PHP_EOL;
+	}
 }
 
-var_dump($plainArray);
-echo $userNode->getId() . ', ' . $userNode->getName() . PHP_EOL;
+usort($fullTargetList, 'compareTarget');
+print_r($fullTargetList);
+
+$serialized = serialize($fullTargetList);
+$fp = fopen('target_list_serialized_ng.dat', 'w');
+fwrite($fp, $serialized);
+fclose($fp);
+
+function compareTarget($a, $b) {
+	return $a['fan_count'] - $b['fan_count'];
+}
---
+++ 1-data-collection/safe_facebook.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SafeFacebook;
+
+use Facebook\Facebook;
+
+class SafeFacebook extends Facebook {
+
+	private $call_count = 0;
+
+	public function get($endpoint, $accessToken = null, $eTag = null, $graphVersion = null) {
+		sleep((int) ($this->call_count / 10));
+		$this->call_count++;
+		return parent::get($endpoint, $accessToken, $eTag, $graphVersion);
+	}
+
+	public function getCallCount() {
+		return $this->call_count;
+	}
+
+}
