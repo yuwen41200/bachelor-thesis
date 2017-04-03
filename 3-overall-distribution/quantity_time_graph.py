ebb75c88fe3f1f681fd3df98eb19ee7673b568c2
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2017-04-04 03:01:31 +0800

    Draw graphs for pA.

--- 3-overall-distribution/quantity_time_graph.py
+++ 3-overall-distribution/quantity_time_graph.py
@@ -5,8 +5,31 @@ from collections import Counter
 
 import dateutil.parser
 import pandas as pd
+import matplotlib.style
+import matplotlib.pyplot as plt
 
 from models import *
+
+
+def render_image(data):
+    plt.clf()
+    ts = pd.Series(data)
+    ax = ts.plot()
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Quantity')
+    plt.gcf().autofmt_xdate()
+    plt.savefig('quantity_time_graph.png', dpi=200, bbox_inches='tight')
+
+
+def render_image_cum(data):
+    plt.clf()
+    ts = pd.Series(data)
+    ts = ts.cumsum()
+    ax = ts.plot()
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Quantity')
+    plt.gcf().autofmt_xdate()
+    plt.savefig('quantity_time_cumulative_graph.png', dpi=200, bbox_inches='tight')
 
 
 def draw():
@@ -15,8 +38,9 @@ def draw():
         time = dateutil.parser.parse(post.time)
         times.append(time.date())
     time_counts = Counter(times)
-    ts = pd.Series(time_counts)
-    ts.plot()
+    render_image(time_counts)
+    render_image_cum(time_counts)
 
 if __name__ == '__main__':
+    matplotlib.style.use('ggplot')
     draw()
